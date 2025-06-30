d4d22a4aa154d9119a732e7902dac5a044f329d9
AmadFat <1943269060@qq.com>
2025-06-30 03:33:18 -0400

    backup

--- src/nnaf_utils/encoding.py
+++ src/nnaf_utils/encoding.py
@@ -2,15 +2,24 @@ from .pytype import *
 
 
 class NodupRehashPool:
-    def __init__(self, pool: set = None):
+    def __init__(
+        self,
+        pool: set = None,
+        hash_func: Callable[[Hashable], str] = None,
+        rehash_func: Callable[[str], str] = None,
+    ):
         self.pool = pool or set()
+        self.hash_func = hash_func
+        self.rehash_func = rehash_func
 
     def __call__(
         self,
         key: Hashable,
-        hash_func: Callable[[Hashable], str] = ...,
-        rehash_func: Callable[[str], str] = ...,
+        hash_func: Callable[[Hashable], str] = None,
+        rehash_func: Callable[[str], str] = None,
     ) -> str:
+        hash_func = self.hash_func or hash_func
+        rehash_func = self.rehash_func or rehash_func
         key = hash_func(key)
         while key in self.pool:
             key = rehash_func(key)
